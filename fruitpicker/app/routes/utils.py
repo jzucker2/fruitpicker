a200bab2534093cd90303faaf1b8e19c5746708c
Jordan Zucker <jordan.zucker@gmail.com>
2024-03-03 22:51:05 -0800

    Wanted to clean this up some more

--- fruitpicker/app/routes/utils.py
+++ fruitpicker/app/routes/utils.py
@@ -20,6 +20,18 @@ def hey():
     return {'message': 'in a bottle'}
 
 
+@app.route('/health')
+@app.route('/api/v1/health')
+@app.route('/utils/health')
+def health_check():
+    log.debug('health route')
+    return {
+        'status': 'healthy',
+        'server': 'fruitpicker',
+        'version': get_version(),
+    }
+
+
 @app.route('/utils/version')
 def version():
     return {
